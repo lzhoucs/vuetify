c11ca512754c6ca5356d9df00ed3256808d0e8bb
Liang Zhou <lzhoucs@gmail.com>
2018-09-05 10:06:29 -0500

    Fixed-column logic and styling.

--- src/components/VDataTable/VDataTable.js
+++ src/components/VDataTable/VDataTable.js
@@ -64,6 +64,7 @@ export default {
   computed: {
     classes () {
       return {
+        'fixed-columns-table': this.getFixedColumnLeft(),
         'datatable table': true,
         'datatable--select-all': this.selectAll !== false,
         'theme--dark': this.dark,
@@ -100,7 +101,11 @@ export default {
   },
 
   render (h) {
-    const tableOverflow = h(VTableOverflow, {}, [
+    const totalFixedWidth = this.getFixedColumnLeft()
+    const tableOverflow = h(VTableOverflow, {
+      // hard code to fix left columns with px unit for now
+      style: totalFixedWidth ? { marginLeft: `${totalFixedWidth}px` } : {}
+    }, [
       h('table', {
         'class': this.classes
       }, [
